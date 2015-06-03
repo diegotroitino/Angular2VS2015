9fc982d7da2315d1e789b040c068e38f4234c317
Diego Troitiño <troits88@gmail.com>
2015-06-02 23:59:37 -0300

    Explore basic features of Angular2 (components, view, for, if)

---
+++ src/Angular2SPA/src/display.js
@@ -0,0 +1,25 @@
+﻿import {ComponentAnnotation as Component, ViewAnnotation as View, NgFor as For, NgIf as If/*, InjectAnnotation as Inject*/} from 'angular2/angular2';
+
+@Component({
+    selector: 'display'//,
+    /*injectables: [FriendsService]*/
+})
+
+@View({    
+    templateUrl: 'display.html',
+    directives: [For, If]
+})
+
+/*@Inject(FriendsService)*/
+export class DisplayComponent {
+    constructor(){//friend) {
+        this.myName = "Diego";
+        this.names = ["Alice", "Aarav", "Martín", "Shannon", "Ariana", "Kai"];//friend.names;
+    }
+}
+
+/*export class FriendsService {
+    constructor() {
+        this.names = ["Alice", "Aarav", "Martín", "Shannon", "Ariana", "Kai"];
+    }
+}*/
--- src/Angular2SPA/src/index.js
+++ src/Angular2SPA/src/index.js
@@ -1,14 +1,16 @@
 import {ComponentAnnotation as Component, ViewAnnotation as View, bootstrap} from 'angular2/angular2';
 import {Angular2App} from 'angular-2-app';
+import {DisplayComponent} from 'display';
 
 @Component({
   selector: 'main'
 })
 
 @View({
-  directives: [Angular2App],
+    directives: [Angular2App, DisplayComponent],
   template: `
     <angular-2-app></angular-2-app>
+    <display></display>
   `
 })
 
